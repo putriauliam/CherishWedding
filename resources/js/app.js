241a5ced2504656ffd97c842fdace7e93ab68008
rahmaalia <rahmaaliaputri27@gmail.com>
2023-12-07 21:51:20 +0700

    navbar pisah

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,3 @@
 import './bootstrap';
 import 'flowbite';
-import './flowbite';
+// import './flowbite';
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,12 +6,14 @@
   @vite('resources/css/app.css','resources/js/app.js')
   @vite(['resources/css/app.css','resources/js/app.js'])
   <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css"  rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/6b738b2603.js" crossorigin="anonymous"></script>
+  <link rel="icon" href="img/logo.png">
 </head>
 <body>
 
-@include('partials.navbar');
+@include('partials.navbar')
 
-<div class="container mt-4">
+<div class="container .mt-1">
   @yield('container')
 </div>
 
