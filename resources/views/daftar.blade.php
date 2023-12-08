fd7bc84e8abddf848776ac4348422962e9134ff2
Hafiz Faiz <hafizhfaiz11@gmail.com>
2023-12-08 08:23:37 +0700

    Merapihkan form daftar dan login

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -13,8 +13,11 @@
   <div class="flex justify-center rounded-3xl">
     <div class="block max-w-sm p-6 bg-white bg-opacity-80 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-96 mt-20">
       <h1 class="text-center mb-2 text-2xl tracking-tight text-gray-900 dark:text-white">DAFTAR</h1>
-      <p>Sudah punya akun? <a href="/masuk" class="text-blue-700">MASUK</a></p>
       <form class="max-w-sm mx-auto">
+      <div class="mb-5">
+          <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Pengguna</label>
+          <input type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        </div>
         <div class="mb-5">
           <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
           <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@example.com" required>
@@ -28,10 +31,10 @@
           <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
         </div>
         <div class="flex flex-col justify-center items-center">
-        <button type="button" class=" text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">DAFTAR</button>
+          <button type="button" class="shadow text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">DAFTAR</button>
+          <p class="pt-4">Sudah punya akun? <a href="/masuk" class="text-blue-700">MASUK</a></p>
         <p class="py-1.5">Atau Daftar Dengan</p>
         <a href="#"><img class="w-20" src="img/google.png" alt=""></a>
-
         </div>
       </form>
     </div>
