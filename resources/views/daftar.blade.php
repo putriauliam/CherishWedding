b16e9695395cc65cc441bab158da7b59d9a1fcb1
rahmaalia <rahmaaliaputri27@gmail.com>
2023-12-08 23:24:48 +0700

    login register google

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -34,7 +34,7 @@
           <button type="button" class="shadow text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">DAFTAR</button>
           <p class="pt-4">Sudah punya akun? <a href="/masuk" class="text-blue-700">MASUK</a></p>
         <p class="py-1.5">Atau Daftar Dengan</p>
-        <a href="#"><img class="w-20" src="img/google.png" alt=""></a>
+        <a href="{{ route('auth.google') }}"><img class="w-20" src="img/google.png" alt=""></a>
         </div>
       </form>
     </div>
