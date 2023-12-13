7dddae3563a6b32ca2628b56b3028a82c4bbc6c2
Hafiz Faiz <hafizhfaiz11@gmail.com>
2023-12-13 20:28:29 +0700

    menambahkan card

--- resources/views/vendor.blade.php
+++ resources/views/vendor.blade.php
@@ -85,6 +85,34 @@
             </div>
         </div>
     </div>
+        <!-- card -->
+        <div class="">
+        <h5 class="mb-2 ml-8 text-2xl font-bold tracking-tight text-gray-900">Semua Kategori</h5>
+        <div class="flex flex-row mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 flex flex-wrap justify-between">
+            <!-- max-w-6xl -->
+                <!-- SubCard -->
+                <div class="card w-44 bg-gray-200 mb-3 mr-2 relative block">
+                        <div class="absolute right-2 mt-4 mr-2 w-10 h-10 flex items-center justify-center rounded-full bg-white border border-gray-200">
+                            <i class="fas fa-heart text-black"></i>
+                        </div>
+                            <img class="p-3 rounded-t-lg" src='https://source.unsplash.com/500x500/?makeup' alt="makeup image" />
+                            <a href="#" class="flex items-center px-5"></a>
+                        <!-- profil vendor -->
+                        <div class="flex-shrink-0 rounded-full overflow-hidden border-2 border-gray-200">
+                            <img class="w-8 h-8" src="img/putri.png" alt="PUTRI">
+                        </div>
+                        <div class="ml-1">
+                            <p class="text-xs font-thin">
+                                Sewa venue Bojongsoang
+                            </p>
+                        </div>
+                    <div class="mt-2 mb-4">
+                        <p class="px-2 flex text-xs font-hard font-thin dark:text-white">VENUE</p>
+                        <p class="px-2 flex text-xs font-medium font-thin dark:text-white">BANDUNG</p>
+                    </div>
+                </div>
+        </div>
+    </div>
 </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
 
