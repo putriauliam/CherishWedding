9cb7d4a41e062926c200911457ee166bd1a5e3c9
putriauliam <ptriauliam61@gmail.com>
2023-12-16 23:05:58 +0700

    Mengerjakan halaman tentang kami

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -1,5 +1,61 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>tentang kami</h1>
+    <div class="text-black mt-32 max-w-xl px-8">
+        <h1 class="text-6xl font-semibold mb-4">Kisah Kami</h1>
+        <p> Dorongan kami dalam menciptakan Cherish Wedding berasal dari keinginan untuk membantu pasangan merencanakan pernikahan mereka dengan lebih lancar, kreatif, dan tanpa tekanan. Dari sinilah lahir sebuah platform yang menghubungkan pasangan dengan vendor-vendor terbaik, sambil tetap mempertahankan fokus pada keindahan, kreativitas, dan kepuasan pelanggan.</p>
+    </div>
+
+     <div class="relative xl:w-1/2 xl:absolute top-32 right-10 ">
+        <img src="img/team.png" class="w-full">
+    </div>
+
+    {{-- <div class="mt-32">
+        <h1 class="text-6xl font-semibold mb-4 text-center">Profil Team</h1>
+    </div> --}}
+
+    <div class="mt-32 text-center">
+        <div class="mt-32 text-center">
+            <h1 class="text-6xl font-semibold mb-12">Profil Team</h1>
+            <div class="flex justify-between mx-16">
+                
+                <div class="flex flex-col items-center">
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="img/putri.png" alt="putri"/>
+                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Putri Aulia Maulidina</h5>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+                
+                <div class="flex flex-col items-center">
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="img/putri.png" alt="putri"/>
+                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Putri Aulia Maulidina</h5>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+
+                <div class="flex flex-col items-center">
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="img/putri.png" alt="putri"/>
+                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Putri Aulia Maulidina</h5>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+
+                <div class="flex flex-col items-center">
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="img/putri.png" alt="putri"/>
+                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Putri Aulia Maulidina</h5>
+                    <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                </div>
+
+            </div>
+        </div>
+        
+    </div>
+    
+
+
+
+ 
+    
+
+
+<script src="https://cdn.tailwindcss.com"></script>
+   
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
 @endsection
