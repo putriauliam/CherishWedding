77d5c121649ab4776365fc82d6bd4377c74a1292
rahmaalia <rahmaaliaputri27@gmail.com>
2023-12-31 15:22:48 +0700

    membenarkan form edit

--- resources/views/dashboard/vendor-post/edit.blade.php
+++ resources/views/dashboard/vendor-post/edit.blade.php
@@ -11,10 +11,10 @@
         {{-- nama --}}
         <div class="mb-2">
             <label for="name" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Nama
-                Lengkap</label>
+                Vendor</label>
             <input type="text" id="name" name="name"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('name') is-invalid @enderror"
-                placeholder="Nama Lengkap"   value="{{ old('name', $vendor->name) }}">
+                placeholder="Nama Vendor"   value="{{ old('name', $vendor->name) }}">
             @error('name')
                 <p class="invalid-feedback mt-2 text-sm text-red-600 dark:text-red-500">
                     {{ $message }}
@@ -25,8 +25,8 @@
         <div class="mb-2">
             <label for="slug" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Slug</label>
             <input type="text" id="slug" name="slug"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('slug') is-invalid @enderror"
-                placeholder="Slug"  value="{{ old('slug', $vendor->slug) }}">
+                class="cursor-not-allowed bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('slug') is-invalid @enderror"
+                placeholder="Slug"  value="{{ old('slug', $vendor->slug) }}" readonly>
             @error('slug')
                 <p class="invalid-feedback mt-2 text-sm text-red-600 dark:text-red-500">
                     {{ $message }}
