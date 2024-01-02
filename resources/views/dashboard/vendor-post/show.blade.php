c1fc0c70a0c0f3236099c4f31892533e77e127e8
Hafiz Faiz <hafizhfaiz11@gmail.com>
2024-01-02 23:27:47 +0700

    memperbaiki typo

--- resources/views/dashboard/vendor-post/show.blade.php
+++ resources/views/dashboard/vendor-post/show.blade.php
@@ -27,15 +27,6 @@
                         <h1>{{ $vendor->name }}</h1>
                     </td>
                 </tr>
-                {{-- slug --}}
-                <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        <label for="slug" class="">Slug</label>
-                    </th>
-                    <td class="px-6 py-4">
-                        <h1>Data</h1>
-                    </td>
-                </tr>
                 {{-- kategori --}}
                 <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
